21f5e82f8aaaabcb2cf542d5e53eb61a61079c60
Akash Giri <akx.sonu@gmail.com>
2024-01-30 22:16:53 +0530

    added node mail

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require("express");
 const hbs = require("hbs");
 const app = express();
 const https = require("https");
+const { url } = require("inspector");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 
 hbs.registerHelper("ifEquals", function (arg1, arg2, options) {
   return arg1 == arg2 ? options.fn(this) : options.inverse(this);
@@ -17,7 +27,7 @@ app.get("/:id", (req, res) => {
   const objectId = parseInt(req.params.id);
 
   https
-    .get("https://13.48.104.206:7240/api/properties/" + objectId, (details) => {
+    .get("https://13.48.104.206:7242/api/properties/" + objectId, (details) => {
       console.log("statusCode:", details.statusCode);
       console.log("headers:", details.headers);
       let data = "";
@@ -36,4 +46,25 @@ app.get("/:id", (req, res) => {
     });
 });
 
+app.post("/enquiry", (req, res) => {
+  const mailTransporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "propsotec@gmail.com",
+      pass: "omzwzybeimeacnfd",
+    },
+  });
+  console.log(req.body);
+  mailTransporter.sendMail(req.body, (err) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ err, status: "failed" });
+    } else {
+      console.log("Mail sent");
+      res.status(200).json({ message: "Mail sent" });
+    }
+  });
+});
+
 app.listen(3000);
+console.log("app started at 3000");
